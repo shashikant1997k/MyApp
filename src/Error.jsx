04c6d9a35767058e1cec5bf8c5681127e38ab7e5
Shashikant <shashikant.1997k@gmail.com>
2020-08-19 14:45:11 +0530

    merge

---
+++ src/Error.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Error.css';
+
+const Error = () =>{
+    return (
+        <>
+            <div id="main">
+                <div class="fof">
+                    <h1>Page Not Found 404</h1>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Error;
--- src/Main.jsx
+++ src/Main.jsx
@@ -3,14 +3,16 @@ import Navbar from './Navbar';
 import RandomImg from './random-img/RandomImg';
 import App from './keep-note/App';
 import { Switch, Route } from 'react-router-dom';
+import Error from './Error';
 
 const Main = () => {
     return (
         <>
            <Navbar />
            <Switch>
-               <Route exact path="/" component={RandomImg} />
-               <Route exact path="/keep-note" component={App} />
+               <Route exact path="/MyApp" component={RandomImg} />
+               <Route exact path="/MyApp/keep-note" component={App} />
+               <Route component={Error} />
            </Switch>
         </>
     )
